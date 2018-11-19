ab52f1cd75445c41757e317da4036e1aa1de44ab
Alexander Pak <pak@mercury.office>
2018-11-19 13:54:28 +0400

    fixed some errors

--- Steganography/Program.cs
+++ Steganography/Program.cs
@@ -9,33 +9,37 @@ namespace Steganography
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            StreamReader sr = new StreamReader("text.txt");
+            Console.OutputEncoding = Encoding.UTF8;
+            StreamReader sr = new StreamReader("../../text.txt");
             Console.WriteLine("Введите слово:");
             var word = Console.ReadLine();
-            byte[] wordBytes = Encoding.Default.GetBytes(word); // Преобразование слова в массив байтов
+            byte[] wordBytes = Encoding.UTF8.GetBytes(word); // Преобразование слова в массив байтов
             List<string> srWords = sr.ReadToEnd().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
             var textWithWord = Encoded(srWords, wordBytes);
+
             var hiddenWord = Decoded(textWithWord);
             Console.WriteLine(hiddenWord);
         }
-
         static string Encoded(List<string> text, byte[] wordBytes)
         {
             string ret = text[0]; // Выходное значение - записываем первое слово
             text.RemoveAt(0); // Удаляем первое слово
-            for(int j = 0; j < wordBytes.Length; j++) // Цикл по байтам
+            foreach (byte _wB in wordBytes) // Цикл по байтам
             {
+                byte wB = _wB;
+                // Console.WriteLine(wB);
                 for (int i = 0; i < 8; i++)    // Цикл по битам
                 {
-                    if ((wordBytes[j] & 0b1) == 0) // Проверяем младший бит
+                    Console.WriteLine(Convert.ToString(wB, 2));
+                    if ((wB & 1) == 0) // Проверяем младший бит
                         ret += " "; // Если 0 - записываем 1 пробел
                     else
                         ret += "  "; // Если 1 - 2 пробела
                     ret += text[0]; // Добавляем следующее слово
                     text.RemoveAt(0); // Удаляем это слово
-                    wordBytes[j] = (byte)(wordBytes[j] >> 1); // Сдвигаем биты байта
+                    wB = (byte)(wB >> 1); // Сдвигаем биты байта
                 }
             }
             ret += "   "; // записываем 3 пробела, когда байты закончились
@@ -59,13 +63,14 @@ namespace Steganography
                     }
                     else if (codedText[ind + 2] != ' ') // Проверка на двойной пробел
                     {
-                        wB = (byte)(wB | 0b1); // Запись 1 в старший бит
-                        indB++;
+                        wB = (byte)(wB | 128); // Запись 1 в старший бит
+                        indB++; ind++;
                     }
                     else break; // Выход из цикла, если тройной пробел
-                    if(indB == 8) // Проверка накопленных битов
+                    // Console.WriteLine(Convert.ToString(wB, 2).PadLeft(8, '0'));
+                    if (indB == 8) // Проверка накопленных битов
                     {
-                        wordB.Add(wB); // Если накоплен бай то его запись
+                        wordB.Add(wB); // Если накоплен байт - его запись
                         wB = 0;
                         indB = 0;
                     }
--- Steganography/Properties/AssemblyInfo.cs
+++ Steganography/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7c6dc9de-ff81-4af4-ab30-9956fdd1406c")]
+[assembly: Guid("41e12a7d-4a29-49bb-a425-29944a0bf994")]
 
 // Version information for an assembly consists of the following four values:
 //
