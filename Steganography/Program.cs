0086f7005b4524a256a55d1e94df5f5f910e779c
lexprom <lexprom009@gmail.com>
2018-12-08 00:19:59 +0400

    Replaced hiding between words by hiding on new line

--- Steganography/Program.cs
+++ Steganography/Program.cs
@@ -13,74 +13,19 @@ namespace Steganography
         {
             Console.OutputEncoding = Encoding.UTF8;
             StreamReader sr = new StreamReader("../../text.txt");
+
             Console.WriteLine("Введите слово:");
             var word = Console.ReadLine();
-            byte[] wordBytes = Encoding.UTF8.GetBytes(word); // Преобразование слова в массив байтов
-            List<string> srWords = sr.ReadToEnd().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
-            var textWithWord = Encoded(srWords, wordBytes);
 
-            var hiddenWord = Decoded(textWithWord);
+            byte[] wordBytes = Encoding.UTF8.GetBytes(word);
+            List<string> srWords = sr.ReadToEnd().Split(new char[] {'\n'}, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            var textWithWord = Steganography.Encoded(srWords, wordBytes);
+            Console.WriteLine(textWithWord);
+
+            var hiddenWord = Steganography.Decoded(textWithWord);
             Console.WriteLine(hiddenWord);
         }
-        static string Encoded(List<string> text, byte[] wordBytes)
-        {
-            string ret = text[0]; // Выходное значение - записываем первое слово
-            text.RemoveAt(0); // Удаляем первое слово
-            foreach (byte _wB in wordBytes) // Цикл по байтам
-            {
-                byte wB = _wB;
-                // Console.WriteLine(wB);
-                for (int i = 0; i < 8; i++)    // Цикл по битам
-                {
-                    Console.WriteLine(Convert.ToString(wB, 2));
-                    if ((wB & 1) == 0) // Проверяем младший бит
-                        ret += " "; // Если 0 - записываем 1 пробел
-                    else
-                        ret += "  "; // Если 1 - 2 пробела
-                    ret += text[0]; // Добавляем следующее слово
-                    text.RemoveAt(0); // Удаляем это слово
-                    wB = (byte)(wB >> 1); // Сдвигаем биты байта
-                }
-            }
-            ret += "   "; // записываем 3 пробела, когда байты закончились
-            ret += string.Join(" ", text); // Добавляем оставшиеся слова исходного списка
-            return ret;
-        }
 
-        static string Decoded(string codedText)
-        {
-            List<byte> wordB = new List<byte>(); // Подготовка списка для байтов слова
-            byte wB = 0; // Для накопления битов
-            int ind = 0; // индекс символов
-            int indB = 0; // индекс битов
-            while (ind < codedText.Length) // Цикл по символам входной строки
-            {
-                if (codedText[ind] == ' ') // Проверка на пробел
-                {
-                    if (codedText[ind + 1] != ' ') // Проверка на одинарный пробел
-                    {
-                        indB++; // Если пробел одиночный - это бит нулевой
-                    }
-                    else if (codedText[ind + 2] != ' ') // Проверка на двойной пробел
-                    {
-                        wB = (byte)(wB | 128); // Запись 1 в старший бит
-                        indB++; ind++;
-                    }
-                    else break; // Выход из цикла, если тройной пробел
-                    // Console.WriteLine(Convert.ToString(wB, 2).PadLeft(8, '0'));
-                    if (indB == 8) // Проверка накопленных битов
-                    {
-                        wordB.Add(wB); // Если накоплен байт - его запись
-                        wB = 0;
-                        indB = 0;
-                    }
-                    wB = (byte)(wB >> 1);
-                }
-                ind++;
-            }
-            string word = Encoding.UTF8.GetString(wordB.ToArray());
-
-            return word;
-        }
     }
 }
---
+++ Steganography/Steganography.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Steganography
+{
+    class Steganography
+    {
+        public static string Encoded(List<string> text, byte[] wordBytes)
+        {
+            string ret = text[0]; //Записываем первое слово
+            text.RemoveAt(0); // Удаляем первое слово
+            foreach (byte _wB in wordBytes) // Цикл по байтам
+            {
+                byte wB = _wB;
+                // Console.WriteLine(wB);
+                for (int i = 0; i < 8; i++)    // Цикл по битам
+                {
+                    // Console.WriteLine(Convert.ToString(wB, 2));
+                    if ((wB & 1) == 0) // Проверяем младший бит
+                        ret += " \n"; // Если 0 - записываем 1 пробел
+                    else
+                        ret += "  \n"; // Если 1 - 2 пробела
+                    ret += text[0]; // Добавляем следующее слово
+                    text.RemoveAt(0); // Удаляем это слово
+                    wB = (byte)(wB >> 1); // Сдвигаем биты байта
+                }
+            }
+            ret += "   \n"; // записываем 3 пробела, когда байты закончились
+            ret += string.Join("\n", text); // Добавляем оставшиеся слова исходного списка
+
+            return ret;
+        }
+
+        public static string Decoded(string codedText)
+        {
+            List<byte> wordB = new List<byte>(); // Подготовка списка для байтов слова
+            byte wB = 0; // Для накопления битов
+            int ind = 0; // индекс символов
+            int indB = 0; // индекс битов
+            while (ind < codedText.Length) // Цикл по символам входной строки
+            {
+                if (codedText[ind] == '\n')
+                {
+                    if (codedText[ind - 1] == ' ') // Проверка на пробел
+                    {
+                        if (codedText[ind - 2] != ' ') // Проверка на одинарный пробел
+                        {
+                            indB++; // Если пробел одиночный - это бит нулевой
+                        }
+                        else if (codedText[ind - 3] != ' ') // Проверка на двойной пробел
+                        {
+                            wB = (byte)(wB | 128); // Запись 1 в старший бит
+                            indB++; ind++;
+                        }
+                        else break; // Выход из цикла, если тройной пробел
+                                    // Console.WriteLine(Convert.ToString(wB, 2).PadLeft(8, '0'));
+                        if (indB == 8) // Проверка накопленных битов
+                        {
+                            wordB.Add(wB); // Если накоплен байт - его запись
+                            wB = 0;
+                            indB = 0;
+                        }
+                        wB = (byte)(wB >> 1);
+                    }
+                }
+                ind++;
+            }
+            string word = Encoding.UTF8.GetString(wordB.ToArray());
+
+            return word;
+        }
+    }
+}
